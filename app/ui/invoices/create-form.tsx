a3b49ac7c97ac0d8401786a4e2b0a296d46a2a51
Varun <varunr.paliwal@gmail.com>
2025-04-22 09:26:50 +0530

    commit ch 14

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -1,17 +1,22 @@
-import { CustomerField } from '@/app/lib/definitions';
-import Link from 'next/link';
+import { createInvoice, State } from "@/app/lib/action";
+import { CustomerField } from "@/app/lib/definitions";
+import { Button } from "@/app/ui/button";
 import {
   CheckIcon,
   ClockIcon,
   CurrencyDollarIcon,
   UserCircleIcon,
-} from '@heroicons/react/24/outline';
-import { Button } from '@/app/ui/button';
-import { createInvoice } from '@/app/lib/action';
+} from "@heroicons/react/24/outline";
+import Link from "next/link";
+import { useActionState } from "react";
 
 export default function Form({ customers }: { customers: CustomerField[] }) {
+  const initialState: State = { message: null, errors: {} };
+
+  const [state, formAction] = useActionState(createInvoice, initialState);
+
   return (
-    <form action={createInvoice}>
+    <form action={formAction}>
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
